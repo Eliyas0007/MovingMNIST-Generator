ca138e68065bf5286016f65a9f6b0615c08b08f1
Eliyas0007 <eliyassuleyman@gmail.com>
2022-03-18 19:44:27 +0000

    Converted to class and uploaded sample image

--- generator.py
+++ generator.py
@@ -8,60 +8,68 @@ import torchvision.transforms as transforms
 from einops import rearrange
 
 
+class Generator():
+    def __init__(self, frame_len=20, step=4, direction='vertical'):
+        
+        '''
+        Direction Options
+        "vertival"
+        "horizontal"
+        "diagonal"
+        '''
+        self.direction = direction 
+        self.step = step
+        self.frame_len = frame_len
 
-mnist_dataset = datasets.MNIST(root='MNISTDATA/', train=False, download=True, transform=transforms.Compose([
+        self._dataset = datasets.MNIST(root='MNISTDATA/', train=False, download=True, transform=transforms.Compose([
                                     transforms.ToTensor(),
                                     ]))
 
 
-frame_len = 20
-step = 4
-
-for item in tqdm.tqdm(range(len(mnist_dataset))):
+    def generate(self):
+        for item in tqdm.tqdm(range(len(self._dataset))):
     
-    image, _ = mnist_dataset[item]
-    image = image.cpu().detach().numpy()
-    i = 0
-    i_p = 0
-    moving_down = True
-    
-    root_path = f'./GeneratedData/video{item}'
-    os.mkdir(root_path)
-    
-    while(i_p < frame_len):
+            image, _ = self._dataset[item]
+            image = image.cpu().detach().numpy()
+            
+            i = 0
+            i_p = 0
+            moving_down = True
 
-        canvas = numpy.zeros((1, 64, 64))
+            root_path = f'./GeneratedData/video{item}_{self.direction}'
+            os.mkdir(root_path)
 
-        if i_p == 0:
-            top = step*i
-            bottom = top + 28
+            while(i_p < self.frame_len):
 
-        if moving_down:
-            i += 1
-            if bottom > 60:
-                moving_down = False
-                continue
-        else:
-            i -= 1
-            if top < 1:
-                moving_down = True
-                continue
+                canvas = numpy.zeros((1, 64, 64))
 
-        top = step*i
-        bottom = top + 28
+                if i_p == 0:
+                    top = self.step*i
+                    bottom = top + 28
 
-        canvas[:, top:bottom, 0:28] += image 
+                if moving_down:
+                    i += 1
+                    if bottom > 60:
+                        moving_down = False
+                        continue
+                else:
+                    i -= 1
+                    if top < 1:
+                        moving_down = True
+                        continue
 
-        
-        canvas = rearrange(canvas, 'c h w -> h w c')
-        image_path = root_path + f'/frame{i_p}.png'
-        # cv2.imshow('asd', canvas)
-        # cv2.waitKey(150)
-        cv2.imwrite(image_path, canvas * 256)
-        canvas = rearrange(canvas, 'h w c -> c h w')
+                top = self.step*i
+                bottom = top + 28
 
-        i_p += 1
+                canvas[:, top:bottom, 0:28] += image 
 
 
+                canvas = rearrange(canvas, 'c h w -> h w c')
+                image_path = root_path + f'/frame{i_p}.png'
+                # cv2.imshow('asd', canvas)
+                # cv2.waitKey(150)
+                cv2.imwrite(image_path, canvas * 256)
+                canvas = rearrange(canvas, 'h w c -> c h w')
 
+                i_p += 1
 
